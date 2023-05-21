06ba333c537686023ddff3c0a2b79689ae8ee7d4
koyalghosh <koyalghosh9928@gmail.com>
2023-05-21 17:46:40 +0530

    Login Page Skeleton

---
+++ WorkFit/app/src/main/java/com/example/workfit/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.workfit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+    }
+}
---
+++ app/src/main/java/com/example/workfit/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.workfit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    var tvSignIn : TextView?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        tvSignIn = findViewById(R.id.signIn)
+        tvSignIn?.setOnClickListener{
+            Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show()
+        }
+    }
+}
