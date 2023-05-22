a9dcbd0d68353bee1aa646d8ff870bb50fbf2bcb
KanishkNoir <kanishksingh.1106@gmail.com>
2023-05-22 21:46:40 +0530

    splash screen

--- app/src/main/java/com/example/workfit/MainActivity.kt
+++ app/src/main/java/com/example/workfit/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     var tvSignIn : TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
