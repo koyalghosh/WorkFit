6b4777928db9fceae4645f35f4a8bfd668b02685
koyalghosh <koyalghosh9928@gmail.com>
2023-05-28 22:23:49 +0530

    BMI result page

--- app/src/main/java/com/example/workfit/WaterActivity.kt
+++ app/src/main/java/com/example/workfit/WaterActivity.kt
@@ -3,15 +3,14 @@ package com.example.workfit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
-import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import com.airbnb.lottie.LottieAnimationView
 
-class MainActivity2 : AppCompatActivity() {
+class WaterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_water)
         val flStartButton : FrameLayout = findViewById(R.id.flStart)
         val animationView: LottieAnimationView = findViewById(R.id.animationView)
         var tvCount : TextView = findViewById(R.id.tvCount)
